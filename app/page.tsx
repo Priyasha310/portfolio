e9b04752476da8b44c6b439ec6bc92a1e8ed188f
Priyasha310 <72645022+Priyasha310@users.noreply.github.com>
2024-03-20 20:54:45 +0530

    font test for deployed version

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Contact from "@/components/Contact/Contact";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Navbar from "@/components/Navbar/Navbar";
-import Projects from "@/components/Projects/Projects";
 import UsedTech from "@/components/UsedTech/UsedTech";
 
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
       <Navbar />
       <Header />
       <About />
-      {/* <Projects/> */}
       <Contact />
       <UsedTech />
       <Footer />
